274c86291b5d0920a8263d24b2d727122b4a20c2
rakaboss98 <54326006+rakaboss98@users.noreply.github.com>
2022-09-20 22:58:59 +0530

    low res optical data generator updated

--- datagenerator/config.py
+++ datagenerator/config.py
@@ -1,4 +1,5 @@
 file_location = dict(
     airbus = '/Users/rakshitbhatt/Documents/GalaxEye /superresolution_gan/rawdata/airbus/',
-    sen2 = '/Users/rakshitbhatt/Documents/GalaxEye /superresolution_gan/rawdata/sentinel-2/'
+    sen2 = '/Users/rakshitbhatt/Documents/GalaxEye /superresolution_gan/rawdata/sentinel-2/',
+    low_res_optical = '/Users/rakshitbhatt/Documents/GalaxEye /superresolution_gan/dataset/low_res_optical/'
 )
--- datagenerator/lowresoptical.py
+++ datagenerator/lowresoptical.py
@@ -3,14 +3,26 @@ import os
 import re
 import rasterio as rio 
 from config import file_location
+import numpy as np
+import matplotlib.pyplot as plt
+import imageio as io 
+import itertools
 
 data_folders = os.listdir(file_location['sen2'])
+
+tile_size = 256
+tiling_stride = int(tile_size/2)
+
+def norm(band):
+    band = band.read(1).astype(np.float32)
+    band = (band-band.min())/(band.max()-band.min())
+    return band
 
 for index, folder in enumerate(data_folders):
     if folder!='.DS_Store':
         img_granule = file_location['sen2'] + folder + '/GRANULE/'
-        temp_file = os.listdir(img_granule)[1]
-        img_granule= img_granule + temp_file + '/IMG_DATA/' + 'R10m/'
+        granule_name = os.listdir(img_granule)[1]
+        img_granule= img_granule + granule_name + '/IMG_DATA/' + 'R10m/'
         bands = os.listdir(img_granule)
         for band in bands:
             if re.search('_B02_10m.jp2',band):
@@ -22,5 +34,22 @@ for index, folder in enumerate(data_folders):
         red = rio.open(red_band)
         blue = rio.open(blue_band)
         green = rio.open(green_band)
-        
+        buffer_x = (red.bounds.right-red.bounds.left)*0.2
+        buffer_y = (red.bounds.top-red.bounds.bottom)*0.2
+        x_left, y_top = red.index(red.bounds.left +buffer_x, red.bounds.top-buffer_y)
+        x_right, y_bott = red.index(red.bounds.right-buffer_x, red.bounds.bottom+buffer_y)
 
+        red = norm(red)[x_left:x_right, y_top:y_bott]
+        blue = norm(blue)[x_left:x_right, y_top:y_bott]
+        green = norm(green)[x_left:x_right, y_top:y_bott]
+        img = np.dstack((red, green, blue))
+
+        height, width, channel = img.shape
+
+        num_x_tiles = int((width-tile_size)/tiling_stride)+1
+        num_y_tiles = int((height-tile_size)/tiling_stride)+1
+
+        for pointer_x, pointer_y in itertools.product(range(num_x_tiles), range(num_y_tiles)):
+            origin_x, origin_y = 0+pointer_x*tiling_stride, 0+pointer_y*tiling_stride
+            cropped_image = img[origin_x:origin_x+tile_size,origin_y:origin_y+tile_size]
+            io.volsave(file_location['low_res_optical']+granule_name+'__'+str(pointer_x)+str(pointer_y)+'.tif', cropped_image)
