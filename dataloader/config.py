d163dfb1c7b15f6876e9123633ca2234510cd1bc
rakaboss98 <54326006+rakaboss98@users.noreply.github.com>
2022-09-26 12:39:37 +0530

    torch dataloader updated

---
+++ dataloader/config.py
@@ -0,0 +1,4 @@
+data_location = dict(
+    low_res_train_data = '/Users/rakshitbhatt/Documents/GalaxEye /superresolution_gan/dataset/low_res_optical/',
+    high_res_train_data = '/Users/rakshitbhatt/Documents/GalaxEye /superresolution_gan/dataset/high_res_optical/'
+)
---
+++ dataloader/load.py
@@ -0,0 +1,58 @@
+import os 
+import imageio.v2 as io
+import random
+import matplotlib.pyplot as plt
+from config import data_location
+from torch.utils.data import Dataset, DataLoader
+from torchvision.transforms import transforms
+
+class LoadItem(Dataset):
+    def __init__(self, root_dir_high_res, root_dir_low_res):
+        self.root_dir_high_res = root_dir_high_res
+        self.root_dir_low_res = root_dir_low_res
+        self.transforms = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.RandomHorizontalFlip(p=0.6),
+            transforms.RandomVerticalFlip(p=0.6)
+        ])
+        self.high_res_files = []
+        self.low_res_files = []
+
+    def __len__(self):
+        self.low_res_files = os.listdir(self.root_dir_low_res)
+        try:
+            self.low_res_files.remove('.DS_Store')
+        except:
+            pass
+
+        self.high_res_files = os.listdir(self.root_dir_high_res)
+        try:
+            self.high_res_files.remove('.DS_Store')
+        except:
+            pass
+
+        self.len_high, self.len_low = len(self.high_res_files), len(self.low_res_files)
+
+        if self.len_high<=self.len_low:
+            return self.len_high
+        else:
+            return self.len_low
+    
+    def __getitem__(self, idx):
+
+        upsamp_image_name = self.high_res_files[idx]
+        id_temp = random.randint(0, self.len_high)
+        downsamp_image_name = self.low_res_files[id_temp]
+
+        downsamp_image = io.imread(self.root_dir_low_res+downsamp_image_name)
+        downsamp_image = self.transforms(downsamp_image)
+        upsamp_image = io.imread(self.root_dir_high_res+upsamp_image_name)
+        upsamp_image = self.transforms(upsamp_image)
+
+        return [downsamp_image, upsamp_image]
+
+if __name__ == "__main__":
+    train_data = LoadItem(data_location['high_res_train_data'], data_location['low_res_train_data'])
+    train_iterator = DataLoader(train_data, shuffle=True, batch_size=2)
+    images = next(iter(train_iterator))
+    print(images[0].shape, images[1].shape)
